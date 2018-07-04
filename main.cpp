ece44274aa11455553a7c339aa627dbe5e9cdb56
Josh01B <37640012+Josh01B@users.noreply.github.com>
2018-07-04 10:51:00 -0600

    Avance en SVG
    Avance en SVG, para impresión de los cambios que va efectuando

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include "ArbolRN.h"
+#include<iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+   int dato = 0;
+   int key = 0;
+
+   ofstream archivo("salida.svg");
+   ArbolRN arbol();
+
+   do{
+      // cout << "Dato:";
+      // cin >> dato;
+      // cout << "Key:";
+	    // cin >> key;
+	    // cout << endl;
+
+
+      /** ingresa los nodos del 4 al 7, para probar la rotación izquierda */
+    //  for(int i=0; i<3; ++i){
+      //  key = i+4;
+      //  dato = key;
+      //  cout << "Ingresa el dato-> Key: " << key << " Dato: " << dato << endl;
+      //  if(key!=-1){
+     	  //   arbol.insertar(key, dato);
+  	     //}
+      //}
+
+      /** ingresa los nodos del 7 al 4, para probar la rotación izquierda */
+      for(int i=0; i<4; ++i){
+        key = 7-i;
+        dato = key;
+        cout << "Ingresa el dato-> Key: " << key << " Dato: " << dato << endl;
+        if(key!=-1){
+     	     arbol.insertar(key, dato);
+  	     }
+      }
+
+      key = -1; /** trabaja junto con el for, para salir del while */
+
+  }while(key != -1);
+
+  cout << "Imprime el arbol: \n" << arbol << endl;
+
+  return 0;
+}
